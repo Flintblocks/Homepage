83bafff333882624100c9a784f92650966c015fc
Syahrul <hello@flintblocks.io>
2023-01-23 15:04:15 +0800

    Code for flintblocks

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,10 +10,52 @@ export const getProjects = async () => {
 };
 
 export const getBio = async () => {
-  const { data } = await axios.get(config.bioUrl);
-
-  return data;
+  return `
+  We're a Web3 Consultancy firm based in Singapore, founder by Syahrul in 2023 and his friends. 
+  We've been active in the space as solo developers since 2017, and took many projects during the DeFi
+  summer of 2020. As a group, we provided smart contract development services to many fledging DeFI protocols.
+  Our core business involve the deployment of decentralized apps and the forking of established protocols,
+  modified to your needs. We can also create a custom protocol just for you!
+  
+  Currently we have successfully forked these protocols and modified them for countless teams:
+  1. Uniswap v1/v2
+  2. Sushiswap (both exchange and masterchef)
+  3. YearnFinance staking pools
+  4. Decentralized perps such as GMX
+  5. ERC20 tokens with custom functionality
+  6. NFTs with custom functionality
+  
+  If you're a new DeFi protocol, we're also provide security audits and optimizations!
+  
+  Type in the \'help'\ command to get a list of all possible commands if you've forgotten!
+  Type in the \'services\' command to get a full range of our services!
+  Type in the \'contact\' command to drop us a message 
+  `;
 };
+
+export const getServices = () => {
+  return `
+  We offer two services, Protocol Design & Deployment and Smart Contract Security Audit
+  Note: While we do both contract development and audit, we do not provide a package that does both.
+  This is to prevent conflicts of interest as an auditor shouldn't be the same as the developer
+  
+  Protocol Design and Development
+  1) Fork any protocol/token of your choice
+     - Token deployment with custom functionality (1 week, USD 1000) 
+     - A well established protocol that we forked before (2 weeks, USD 5000)
+     - A well established protocol that we never forked before (4 weeks, USD 10000)
+  2) Create your own protocol
+     - Develop your own pools
+     - New ideas that you're looking to explore
+     
+  Security Audit
+  We will audit your smart contract. Our audit rates are fixed, at 5000 USD per project. 
+  Our audits will include: 
+  1) Gas optimizations ( we will provide a report on how much gas is saved)
+  2) Style optimizations
+  3) Vulnerability Report (low,medium,high and critical severities)
+  `
+}
 
 export const getWeather = async (city: string) => {
   const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -7,80 +7,32 @@ export const help = async (args: string[]): Promise<string> => {
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
-export const echo = async (args: string[]): Promise<string> => {
-  return args.join(' ');
+
+export const contact = async (args: string[]): Promise<string> => {
+  return 'Drop us an email message at hello@flintblocks.io to get in touch and discuss what we can do for you';
 };
 
-export const whoami = async (args: string[]): Promise<string> => {
-  return 'guest';
-};
 
-export const date = async (args: string[]): Promise<string> => {
-  return new Date().toString();
-};
-
-export const gui = async (args: string[]): Promise<string> => {
-  window.open('https://m4tt72.com', '_self');
-
-  return 'Opening GUI version...';
-};
-
-export const email = async (args: string[]): Promise<string> => {
-  window.open('mailto:hi@nm4tt72.com');
-
-  return 'Opening mailto:hi@m4tt72.com...';
-};
-
-export const vi = async (args: string[]): Promise<string> => {
-  return `why use vi? try 'emacs'.`;
-};
-
-export const vim = async (args: string[]): Promise<string> => {
-  return `why use vim? try 'emacs'.`;
-};
-
-export const emacs = async (args?: string[]): Promise<string> => {
-  return `really? emacs? you should be using 'vim'`;
-};
-
-export const sudo = async (args?: string[]): Promise<string> => {
-  setTimeout(function () {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  }, 1000);
-
-  return `Permission denied: unable to run the command '${args[0]}' as root.`;
-};
-
-export const repo = async (args?: string[]): Promise<string> => {
-  setTimeout(function () {
-    window.open('https://github.com/m4tt72/terminal', '_blank');
-  }, 1000);
-
-  return 'Opening repository...';
-};
-
-export const donate = async (args?: string[]): Promise<string> => {
-  window.open(packageJson.funding.url, '_blank');
-
-  return 'Opening donation url...';
-};
 
 export const banner = (args?: string[]): string => {
   return `
-███╗   ███╗██╗  ██╗████████╗████████╗███████╗██████╗
-████╗ ████║██║  ██║╚══██╔══╝╚══██╔══╝╚════██║╚════██╗
-██╔████╔██║███████║   ██║      ██║       ██╔╝ █████╔╝
-██║╚██╔╝██║╚════██║   ██║      ██║      ██╔╝ ██╔═══╝
-██║ ╚═╝ ██║     ██║   ██║      ██║      ██║  ███████╗
-╚═╝     ╚═╝     ╚═╝   ╚═╝      ╚═╝      ╚═╝  ╚══════╝ v${packageJson.version}
-
-Type 'help' to see list of available commands.
-
+   ____ __   __ _  __ _____ ___   __   _    __  _     ___
+  / __// /  / // |/ //_  _// o.) / / ,' \\ ,'_/ / //7,' _/
+ / _/ / /_ / // || /  / / / o \\ / /_/ o |/ /_ /  ,'_\\ \`. 
+/_/  /___//_//_/|_/  /_/ /___,'/___/|_,' |__//_/\\\\/___,' 
+                                                         v${packageJson.version}
 --
-The project is open-source 🎉 type 'repo' to check out the repository.
+Flintblocks is a web3 consultancy firm based in Singapore 🦁
+We're founded in 2023 in the year of the rabbit 🐇
+Here are the list of available commands to get you started 💯💯
 
-New 🎉: Try out the new 'theme' command. See all available themes <a href="https://github.com/m4tt72/terminal/tree/master/docs/themes">in the docs</a>.
-New 🎉: New command 'neofetch', for you linux.
+Type any of this commands in this website ⌨
+
+[Commands] 
+about - Bring up the about page
+contact - Show how to drop us a message
+projects - All the projects that we've done so far
+services - What we offer to founders creating their new DeFi projects!
 --
 `;
 };
